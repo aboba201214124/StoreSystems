173f55118bcce476aa8b8ff122f9c51c0cd07c32
student <straffic444@mail.ru>
2024-12-09 12:36:16 +0700

    09.12

--- create.php
+++ create.php
@@ -7,14 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<style>
-    a{
-        margin: 20px;
-    }
-</style>
 <body>
-<a href="create.php">Список товаров</a>
-<a href="">Добавить товар в базу</a>
-<a href="">Добавить информацию о поступлении товара</a>
+
 </body>
 </html>
